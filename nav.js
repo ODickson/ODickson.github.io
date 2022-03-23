71f3aa29f386ae6ce35f840c653597c69c508ea6
ODickson <o.b.p.dickson@live.com>
2022-03-23 22:54:42 +0000

    updating todays changes

---
+++ nav.js
@@ -0,0 +1,7 @@
+
+document.write('<a href="index.html">Homepage</a>');
+document.write('<a href="experience.html">Experience</a>');
+document.write('<a href="bioinformatics_home.html">Bioinformatics Projects</a>');
+document.write('<a href="dats_science_home.html">Data Science Projects</a>');
+document.write('<a href="blog_posts_home.html">Blog Posts</a>');
+
