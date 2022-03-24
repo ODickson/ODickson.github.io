edcb03051c1918334d895ec69a5abfed036e184e
ODickson <o.b.p.dickson@live.com>
2022-03-24 19:07:02 +0000

    update

---
+++ fotter.js
@@ -0,0 +1,67 @@
+const htmlstring = `
+    <div class="container">
+        <div class="row">
+            <div class="col-8 col-12-medium"> -->
+                <!-- Links -->
+                    <!-- <section>
+                        <h2>Links to Important Stuff</h2>
+                        <div>
+                            <div class="row">
+                                <div class="col-3 col-12-small">
+                                    <ul class="link-list last-child">
+                                        <li><a href="#">Neque amet dapibus</a></li>
+                                        <li><a href="#">Sed mattis quis rutrum</a></li>
+                                        <li><a href="#">Accumsan suspendisse</a></li>
+                                        <li><a href="#">Eu varius vitae magna</a></li>
+                                    </ul>
+                                </div>
+                                <div class="col-3 col-12-small">
+                                    <ul class="link-list last-child">
+                                        <li><a href="#">Neque amet dapibus</a></li>
+                                        <li><a href="#">Sed mattis quis rutrum</a></li>
+                                        <li><a href="#">Accumsan suspendisse</a></li>
+                                        <li><a href="#">Eu varius vitae magna</a></li>
+                                    </ul>
+                                </div>
+                                <div class="col-3 col-12-small">
+                                    <ul class="link-list last-child">
+                                        <li><a href="#">Neque amet dapibus</a></li>
+                                        <li><a href="#">Sed mattis quis rutrum</a></li>
+                                        <li><a href="#">Accumsan suspendisse</a></li>
+                                        <li><a href="#">Eu varius vitae magna</a></li>
+                                    </ul>
+                                </div>
+                                <div class="col-3 col-12-small">
+                                    <ul class="link-list last-child">
+                                        <li><a href="#">Neque amet dapibus</a></li>
+                                        <li><a href="#">Sed mattis quis rutrum</a></li>
+                                        <li><a href="#">Accumsan suspendisse</a></li>
+                                        <li><a href="#">Eu varius vitae magna</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+            </div> -->
+            <!-- <div class="col-4 col-12-medium imp-medium"> -->
+
+                <!-- Blurb -->
+                    <!-- <section>
+                        <h2>An Informative Text Blurb</h2>
+                        <p align="center">This site acts as a portfolio and repository for my code/projects. Feel free to reach 
+                        out to me at <a href=mailto: "o.b.p.dickson@live.com" ><font color="white">o.b.p.dickson@live.com</font></a>
+                        </p>
+                    </section>
+
+            </div>
+        </div>
+    </div>
+</section> -->
+
+<!-- Copyright -->
+<!-- <div id="copyright">
+    &copy; Untitled. All rights reserved. | Design: <a href="http://html5up.net">HTML5 UP</a>
+</div>
+`;
+document.write(htmlstring)
