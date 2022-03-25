806354f17b26fea5dd3345761a126d0529f79a04
ODickson <o.b.p.dickson@live.com>
2022-03-25 12:41:42 +0000

    updates

---
+++ footer.js
@@ -0,0 +1,25 @@
+const htmlstring = `
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-12-medium imp-medium">
+
+                <!-- Blurb -->
+                    <section>
+                        <p align="center" style="font-size: 1.5em" >This site acts as a portfolio and repository for my code/projects. Feel free to reach 
+                        out to me at <a href=mailto: "o.b.p.dickson@live.com" >o.b.p.dickson@live.com</font></a> if you have any questions or comments.
+                        Check out my LinkedIn here: <a class="badge-base__link LI-simple-link" href="https://uk.linkedin.com/in/oliver-dickson-a0a941218?trk=profile-badge">Oliver Dickson</a>
+                        </div>
+                        </p>
+                    </section>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Copyright -->
+<div id="copyright">
+    Oliver Dickson&copy;. All rights reserved. | Design: adapted from a <a href="http://html5up.net">HTML5 UP</a> design.
+</div>
+`;
+document.write(htmlstring)
