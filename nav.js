ac6c544568f88563ae5a6be5461a9624b59f7a27
ODickson <o.b.p.dickson@live.com>
2022-03-29 09:31:50 +0100

    final template finished

--- nav.js
+++ nav.js
@@ -1,8 +1,8 @@
 
 document.write('<a href="index.html">Homepage</a>');
 document.write('<a href="experience.html">My experience</a>');
-document.write('<a href="bioinformatics_home.html">Bioinformatics</a>');
-document.write('<a href="dats_science_home.html">Data Science</a>');
-document.write('<a href="blog_posts_home.html">Blog</a>');
+document.write('<a href="bioinformatics_home.html">Bioinformatics Projects</a>');
+document.write('<a href="data_science_home.html">Data Science Projects</a>');
+// document.write('<a href="blog_posts_home.html">Blog</a>');
 document.write('<a href="contact.html">Contact Me</a>');
 
