8b332829d5b8cf1282fe773f7fa9513de347fd0c
ODickson <o.b.p.dickson@live.com>
2022-03-29 21:51:01 +0100

    main body of website is ready

--- footer.js
+++ footer.js
@@ -1,25 +1,29 @@
 const htmlstring = `
-    <div class="container">
-        <div class="row">
-            <div class="col-12 col-12-medium imp-medium">
-
-                <!-- Blurb -->
-                    <section>
-                        <p align="center" style="font-size: 1.5em" >This site acts as a portfolio and repository for my code/projects. Feel free to reach 
-                        out to me at <a href="mailto: o.b.p.dickson@live.com" >o.b.p.dickson@live.com</font></a> if you have any questions or comments.
-                        Check out my LinkedIn here: <a href="https://uk.linkedin.com/in/oliver-dickson-a0a941218?trk=profile-badge">Oliver Dickson</a>
+                <section id="footer">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12 col-12-medium imp-medium">
+                                <!-- Blurb -->
+                                <section>
+                                    <p align="center" style="font-size: 1.5em" >This site acts as a portfolio and repository for my code/projects. Feel free to reach 
+                                    out to me at <a href="mailto: o.b.p.dickson@live.com" >o.b.p.dickson@live.com</font></a> if you have any questions or comments.
+                                    Check out my LinkedIn here: <a href="https://uk.linkedin.com/in/oliver-dickson-a0a941218?trk=profile-badge">Oliver Dickson</a>
+                                    </p>
+                                </section>
+                            </div>
                         </div>
-                        </p>
-                    </section>
+                    </div>
+                </section>
 
-            </div>
-        </div>
-    </div>
-</section>
-
-<!-- Copyright -->
-<div id="copyright">
-    Oliver Dickson&copy;. All rights reserved. | Design: adapted from a <a href="http://html5up.net">HTML5 UP</a> design.
-</div>
-`;
+                <!-- Copyright -->
+                <div id="copyright">
+                    Oliver Dickson&copy;. All rights reserved. | Design: adapted from a <a href="http://html5up.net">HTML5 UP</a> design.
+                </div>
+                <!-- Scripts -->
+                    <script src="/assets/js/jquery.min.js"></script>
+                    <script src="/assets/js/browser.min.js"></script>
+                    <script src="/assets/js/breakpoints.min.js"></script>
+                    <script src="/assets/js/util.js"></script>
+                    <script src="/assets/js/main.js"></script>
+                `;
 document.write(htmlstring)
